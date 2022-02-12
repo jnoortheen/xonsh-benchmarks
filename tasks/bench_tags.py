2bb85dbb84a855f89d99e1934eba2c5d35f90947
Noortheen Raja <jnoortheen@gmail.com>
2022-02-12 14:29:52 +0530

    feat: add bench for  latest tags

---
+++ tasks/bench_tags.py
@@ -0,0 +1,27 @@
+import subprocess as sp
+
+import requests
+from tqdm import tqdm
+
+
+def get_tags():
+    count = 10  # run for latest 10 tags
+    resp = requests.get("https://api.github.com/repos/xonsh/xonsh/tags").json()
+    for obj in resp:
+        count -= 1
+        yield obj["name"]
+        if count < 0:
+            break
+    yield "main"
+
+
+def main():
+    tags = list(get_tags())
+    for tag in tqdm(tags):
+        args = ["asv", "run", "--skip-existing", "--profile", f"{tag}^!"]
+        print(f"${args}")
+        sp.run(args)
+
+
+if __name__ == "__main__":
+    main()
