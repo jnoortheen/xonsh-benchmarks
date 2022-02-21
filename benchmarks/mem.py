eb2e0214c4b847c4e464e4a2f916cf075ec66c86
Noortheen Raja <jnoortheen@gmail.com>
2022-02-21 11:00:35 +0530

    feat: add pympler to track memory

--- benchmarks/mem.py
+++ benchmarks/mem.py
@@ -1,11 +1,33 @@
 """individual component's memory usage"""
 
 
-def mem_parser():
+def _get_parser():
     from xonsh.parser import Parser
 
     p = Parser()
     # wait for thread to finish
     p.parse("1")
-
     return p
+
+
+def _in_detail(more_info=False):
+    from pympler import asizeof
+
+    p = _get_parser()
+
+    if more_info:
+        print(asizeof.asized(p, detail=2).format())
+        # print detailed memory usage of the ply yacc
+        print(asizeof.asized(p.parser, detail=2).format())
+
+    # size of the combined object in bytes
+    return asizeof.asizeof(p)
+
+
+def track_parser_size():
+    """use pympler to the size of the parser including all of its attributes"""
+    return _in_detail()
+
+
+if __name__ == "__main__":
+    print(_in_detail(True))
--- benchmarks/peakmem.py
+++ benchmarks/peakmem.py
@@ -8,21 +8,34 @@ def _inp_exit():
     from tempfile import NamedTemporaryFile
     from unittest import mock
 
-    with NamedTemporaryFile(mode="w+") as inp_file:
-        with mock.patch("sys.stdin", inp_file):
-            inp_file.write("exit\n")
-            inp_file.seek(0)
-            yield inp_file
+    with NamedTemporaryFile(mode="w+") as inp, NamedTemporaryFile(
+        mode="w+"
+    ) as out, NamedTemporaryFile(mode="w+") as err:
+        with (
+            mock.patch("sys.stdin", inp),
+            # mock.patch("sys.stdout", out),
+            mock.patch("sys.stderr", err),
+        ):
+            inp.write("exit\n")
+            inp.seek(0)
+            yield inp, out, err
+
+        def read(file):
+            file.seek(0)
+            return file.read()
+
+        assert "exit" in read(inp)
+        # print(f"{read(inp)=}, {read(out)=}, {read(err)=}")
 
 
 @contextmanager
 def _mock_rl():
     from unittest import mock
 
-    with _inp_exit(), mock.patch(
+    with _inp_exit() as inp, mock.patch(
         "xonsh.readline_shell.ReadlineShell._load_remaining_input_into_queue"
     ):
-        yield
+        yield inp
 
 
 def peakmem_script():
@@ -35,21 +48,24 @@ def peakmem_script():
 
 
 def peakmem_interactive_rl():
-    from xonsh.main import main
-
     with _mock_rl():
+        from xonsh.main import main
+
         try:
-            main(["-i", "--shell=rl"])
+            main(["-i", "--shell=rl", "--no-rc"])
         except SystemExit:
             return
 
 
-def peakmem_interactive_ptk():
-    from xonsh.main import main
+peakmem_interactive_rl.timeout = 10.0  # in seconds
 
+
+def peakmem_interactive_ptk():
     with _inp_exit():
+        from xonsh.main import main
+
         try:
-            main(["-i", "--shell=ptk"])
+            main(["-i", "--shell=ptk", "--no-rc"])
         except SystemExit:
             return
 
